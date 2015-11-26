5a47ac374613f92d3a6a25874154cd60ef88854b
Stefan Kloepping <northalpha@c3pb.de>
2015-11-26 17:58:12 +0100

    Light-LCD.py: fixed indenting

--- light-LCD.py
+++ light-LCD.py
@@ -1,27 +1,27 @@
-	#!/usr/bin/python
-	#
-	# Stefan Kloepping <northalpha@c3pb.de>
-	#  --  Do 26 Nov 17:27:27 2015
-	#
-	import pyupm_i2clcd as lcd
-	import pyupm_grove
-	import time
+#!/usr/bin/python
+#
+# Stefan Kloepping <northalpha@c3pb.de>
+#  --  Do 26 Nov 17:27:27 2015
+#
+import pyupm_i2clcd as lcd
+import pyupm_grove
+import time
 
-	# define that Light Sensor is on A0
-	light = pyupm_grove.GroveLight(0)
+# define that Light Sensor is on A0
+light = pyupm_grove.GroveLight(0)
 
-	# initiale LCD display
-	myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+# initiale LCD display
+myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
 
-	while True:
+while True:
 		
-		# Green
-		myLcd.setColor(255, 255, 0)
+	# Green
+	myLcd.setColor(255, 255, 0)
 
-		# Zero the cursor
-		myLcd.setCursor(0,0)
+	# Zero the cursor
+	myLcd.setCursor(0,0)
 
-		# Print first line with light value in Lux:
-		text = str('Lux Wert: ') + str(light.value())
-		myLcd.write(text)
-		time.sleep(1)
+	# Print first line with light value in Lux:
+	text = str('Lux Wert: ') + str(light.value())
+	myLcd.write(text)
+	time.sleep(1)
